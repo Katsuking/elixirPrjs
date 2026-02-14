0f6aeaf827ad80a649d920fd342bb9bb7396b710
Katsuking <legendsoccerforte5@gmail.com>
2026-02-15 08:39:38 +0900

    chore

--- playground/hello_liveview/lib/hello_liveview_web/components/layouts.ex
+++ playground/hello_liveview/lib/hello_liveview_web/components/layouts.ex
@@ -38,8 +38,8 @@ defmodule HelloLiveviewWeb.Layouts do
     <header class="navbar px-4 sm:px-6 lg:px-8">
       <div class="flex-1">
         <a href="/" class="flex-1 flex w-fit items-center gap-2">
-          <img src={~p"/images/logo.svg"} width="36" />
-          <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
+          <img src={~p"/images/check.svg"} width="30" class="-translate-y-0.5" />
+          <span class="text-sm font-semibold">Learn Averywhere</span>
         </a>
       </div>
       <div class="flex-none">
